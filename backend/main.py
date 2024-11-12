0f28c7de1765abc4e38dc45e939d2677ee00f5c9
P-nasi <pnasimonan@gmail.com>
2024-11-12 20:57:26 +0900

    initial comit

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+"""
+バックエンド（自動入力して予約する部分）
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import flask
+import sys
+import time
+
+def reserve():
+    pass
